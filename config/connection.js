a99b6daa869955ab3f8a24bb2e7e9121545c483f
Kevin Wu <peiyuwu9@gmail.com>
2019-11-05 20:07:29 -0500

    Update connection config

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+// Sets up db to connect locally or on JAWSDB if deployed
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "",
     database: "burgers_db"
-});
+  });
+}
 
 connection.connect(function (err) {
     if (err) {
